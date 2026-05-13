fd30936a636916ddda8600d346a2af63da8a4479
Fityulane <gyorgyi.fityo@gmail.com>
2026-05-13 13:09:55 +0200

    Képernyők előkészítése
    Képernyők előkészítése a navigáció alapokhoz.
    - Menü, új játék, betöltés
    - Fő játék képernyő
    - Karakterlap

---
+++ app/src/main/java/hu/nje/kalandjatekmobil/ui/LoadgameFragment.java
@@ -0,0 +1,25 @@
+package hu.nje.kalandjatekmobil.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hu.nje.kalandjatekmobil.R;
+
+//Játékállás betöltése
+
+public class LoadgameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_loadgame, container, false);
+
+        return view;
+    }
+}
