ce420d37390a9060591c221ecdd6dbb98339f26a
Csermat <38727393+Csermat@users.noreply.github.com>
2026-05-16 04:54:20 +0200

    Navigáció alapozása

--- app/src/main/java/hu/nje/kalandjatekmobil/MainActivity.java
+++ app/src/main/java/hu/nje/kalandjatekmobil/MainActivity.java
@@ -1,26 +1,70 @@
 package hu.nje.kalandjatekmobil;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Navigáció előkészítés:
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Toolbar beállítás
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+
+        //Címsor beállítás
+        setTitle("Kalandjáték");
+        //Navigáció beállítása
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+        // Top level destinations
+        // Top level destinations beállítás
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.fragmentHome, R.id.fragmentCharacter
+        ).build();
+
+        NavigationUI.setupActionBarWithNavController(
+                this, navController, appBarConfiguration
+        );
     }
 
-    //Test
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.fragmentCharacter){
+            showMessage("Karakterlap");
+        }
+        //return true;
+        return NavigationUI.onNavDestinationSelected(item,navController);
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
 }
